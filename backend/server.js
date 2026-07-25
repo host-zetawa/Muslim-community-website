7e1b2e7aea52f7e1685d2f7848d6f179f24d4047
Tanisha <tanishamahajan2310@gmail.com>
2026-07-25 16:14:45 +0530

    increased json limit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ console.log(JWT_SECRET);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 const PORT = 3000;
 
 
